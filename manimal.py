88d277927d965744b0b4d2fdb7aa03e60a016741
Tim Band <t.band@nnnn.com>
2022-01-20 13:39:00 +0000

    sliding image visible

--- manimal.py
+++ manimal.py
@@ -21,6 +21,7 @@ def emptyQueue(queue):
     except:
         pass
 
+
 def readImage(queue, czi: CziFile, radius : int, zoom, cx : int, cy : int):
     """
     parameters:
@@ -46,41 +47,150 @@ def readImage(queue, czi: CziFile, radius : int, zoom, cx : int, cy : int):
     rect = (left, top, width, height)
     data = czi.read_mosaic(rect, scale, C=0)
     print("rect: ({0},{1},{2},{3}) scale: {4}".format(top, left, width, height, scale))
-    img = np.asarray(np.multiply(data[0], 1.0/256.0).astype(np.uint8))
+    brightness = 2.0
+    rescaled = np.minimum(np.multiply(data[0], brightness/256.0), 256.0)
+    img = np.asarray(rescaled.astype(np.uint8))
     pil = Image.fromarray(img)
     emptyQueue(queue)
     queue.put((pil, rect, zoom))
     print("read")
 
+class ZoomableImage:
+    def __init__(self, cziPath):
+        self.czi = CziFile(pathlib.Path(cziPath))
+        self.queue = queue.SimpleQueue()
+        self.bbox = self.czi.get_mosaic_bounding_box()
+        self.pil = None
+        self.pilPosition = None
+        self.zoom = 100
+
+    def centre(self):
+        return (self.bbox.x + self.bbox.w // 2, self.bbox.y + self.bbox.h // 2)
+
+class ImageReadRequest:
+    def __init__(self, image: ZoomableImage, radius: int, zoom, cx: int, cy:int):
+        self.queue = image.queue
+        self.czi = image.czi
+        self.radius = radius
+        self.zoom = zoom
+        self.cx = cx
+        self.cy = cy
+
+def readImages(inputQueue):
+    while True:
+        i = inputQueue.get()
+        readImage(i.queue, i.czi, i.radius, i.zoom, i.cx, i.cy)
+
+class TransformableImage:
+    def __init__(self, zim: ZoomableImage, cx: int, cy: int, zoom):
+        self.zim = zim
+        self.cx = cx
+        self.cy = cy
+        self.zoom = zoom
+
+    def getCropped(self, width, height):
+        # position on the fixed background
+        zwidth = (width * 100) / self.zoom
+        zheight = (height * 100) / self.zoom
+        zleft = math.floor(self.cx - zwidth / 2 + 0.5)
+        ztop = math.floor(self.cy - zheight / 2 + 0.5)
+        zwidth = math.floor(zwidth)
+        zheight = math.floor(zheight)
+        # position of cropped image on screen
+        offsetX = 0
+        offsetY = 0
+        # crops in background pixels
+        bbox = self.zim.bbox
+        cropped = False
+        leftPil = self.zim.pilPosition[0]
+        leftCrop = zleft - leftPil
+        if leftCrop < 0:
+            offsetX = -leftCrop
+            leftCrop = 0
+            cropped = bbox.x < leftPil
+        topPil = self.zim.pilPosition[1]
+        topCrop = ztop - topPil
+        if topCrop < 0:
+            offsetY = -topCrop
+            topCrop = 0
+            cropped = cropped or bbox.y < topPil
+        rightCrop = zleft + zwidth - leftPil
+        widthPil = self.zim.pilPosition[2]
+        if widthPil < rightCrop:
+            rightCrop = widthPil
+            cropped = cropped or leftPil + widthPil < bbox.x + bbox.w
+        bottomCrop = ztop + zheight - topPil
+        heightPil = self.zim.pilPosition[3]
+        if heightPil < bottomCrop:
+            bottomCrop = heightPil
+            cropped = cropped or topPil + heightPil < bbox.y + bbox.h
+        print("window ({0},{1}) {2}x{3}".format(zleft, ztop, zwidth, zheight))
+        print("crop ({0},{1},{2},{3})".format(leftCrop, topCrop, rightCrop, bottomCrop))
+        widthCrop = rightCrop - leftCrop
+        heightCrop = bottomCrop - topCrop
+        if widthCrop <= 0 or heightCrop <= 0:
+            return None
+        # width and height in screen pixels
+        canvasX = math.floor(offsetX * self.zoom / 100)
+        canvasY = math.floor(offsetY * self.zoom / 100)
+        canvasW = math.ceil(widthCrop * self.zoom / 100)
+        canvasH = math.ceil(heightCrop * self.zoom / 100)
+        # crops in extracted image pixels
+        topIm = max(0, math.floor(topCrop * self.zim.zoom / 100))
+        bottomIm = min(self.zim.pil.height, math.floor(bottomCrop * self.zim.zoom / 100))
+        leftIm = max(0, math.floor(leftCrop * self.zim.zoom / 100))
+        rightIm = min(self.zim.pil.width, math.floor(rightCrop * self.zim.zoom / 100))
+        print("canvas ({0},{1}) image ({2},{3},{4},{5})".format(canvasW, canvasH, leftIm, topIm, rightIm, bottomIm))
+        self.image = self.zim.pil.resize(
+            (canvasW, canvasH),
+            resample=Image.NEAREST,
+            box=(leftIm, topIm, rightIm, bottomIm)
+        )
+        cropped = cropped
+        self.x = canvasX
+        self.y = canvasY
+        magnification = self.zoom / self.zim.zoom
+        self.outdated = (magnification <= 0.5
+            or (2 <= magnification and self.zim.zoom < 100)
+            or cropped)
+        return self.image
+
+    def get(self, width, height):
+        im = self.getCropped(width, height)
+        if (im != None
+            and self.x == 0 and self.y == 0
+            and im.width == width and im.height == height
+        ):
+            return im
+        bg = Image.new("RGB", (width, height), (0,0,0))
+        if im != None:
+            bg.paste(im, (self.x, self.y))
+        return bg
+
 class ManimalApplication(tk.Frame):
     def __init__(self, master, fixed, sliding):
         super().__init__(master)
-        self.fixedImageQueue = queue.SimpleQueue()
         self.grid(row=0, column=0, sticky="nsew")
         self.okButton = tk.Button(self, text="OK", command = self.saveAndQuit)
         master.bind('<Return>', lambda e: self.saveAndQuit())
         self.cancelButton = tk.Button(self, text="cancel", command = self.quit)
         master.bind('<Escape>', lambda e: self.quit())
-        self.fixedImage = CziFile(pathlib.Path(fixed))
-        self.slidingImage = CziFile(pathlib.Path(sliding))
+        self.fixed = ZoomableImage(pathlib.Path(fixed))
+        self.sliding = ZoomableImage(pathlib.Path(sliding))
         self.canvas = tk.Canvas(self)
-        fibb = self.fixedImage.get_mosaic_bounding_box()
-        sibb = self.slidingImage.get_mosaic_bounding_box()
         # centres of the images
-        self.centreFixedX = fibb.x + fibb.w // 2
-        self.centreFixedY = fibb.y + fibb.h // 2
-        self.centreSlidingX = sibb.x + sibb.w // 2
-        self.centreSlidingY = sibb.y + sibb.h // 2
+        centreFixed = self.fixed.centre()
+        centreSliding = self.sliding.centre()
         # rotation (in radians) then translation (in image pixels) of the sliding image
         self.rotation = 0.0
-        self.slideX = self.centreFixedX - self.centreSlidingX
-        self.slideY = self.centreFixedY - self.centreSlidingY
+        self.slideX = centreFixed[0] - centreSliding[0]
+        self.slideY = centreFixed[1] - centreSliding[1]
         # zoom percent
         self.zoomLevels = [5, 10, 25, 50, 100, 200, 400]
         self.zoom = 100
         # where the centre of the view is on the fixed image
-        self.panX = self.centreFixedX
-        self.panY = self.centreFixedY
+        self.panX = centreFixed[0]
+        self.panY = centreFixed[1]
         # None if we are rotating and sliding in "free" mode, an (x,y)
         # co-ordinate pair about which we are rotating if we are in
         # "pinned" mode.
@@ -99,8 +209,12 @@ class ManimalApplication(tk.Frame):
         self.canvas.bind("<MouseWheel>", lambda e: self.zoomChange(e.delta, e.x, e.y))
         self.canvas.bind("<Button-4>", lambda e: self.zoomChange(-120, e.x, e.y))
         self.canvas.bind("<Button-5>", lambda e: self.zoomChange(120, e.x, e.y))
-        self.fixedImagePil = None
-        self.fixedImageThread = None
+        self.requestQueue = queue.SimpleQueue()
+        self.readThread = threading.Thread(
+            target=readImages, args=(self.requestQueue,)
+        )
+        self.readThread.daemon = True
+        self.readThread.start()
         self.update_idletasks()
         self.readCurrentLocality()
         self.createCanvas()
@@ -108,19 +222,24 @@ class ManimalApplication(tk.Frame):
 
     def readCurrentLocality(self):
         print("read")
-        if self.fixedImageThread != None and self.fixedImageThread.is_alive():
+        if not self.requestQueue.empty():
             print("busy")
             return
         radius = self.canvas.winfo_width() + self.canvas.winfo_height()
-        self.fixedImageThread = threading.Thread(target=readImage, args=(
-            self.fixedImageQueue,
-            self.fixedImage,
+        self.requestQueue.put(ImageReadRequest(
+            self.fixed,
             math.floor(radius * 100 / self.zoom),
             self.zoom,
             self.panX,
             self.panY
         ))
-        self.fixedImageThread.start()
+        self.requestQueue.put(ImageReadRequest(
+            self.sliding,
+            math.floor(radius * 100 / self.zoom),
+            self.zoom,
+            self.panX - self.slideX,
+            self.panY - self.slideY
+        ))
         print("go")
 
     def saveAndQuit(self):
@@ -183,72 +302,37 @@ class ManimalApplication(tk.Frame):
         # sliding image: rotate -> slide -> -pan -> zoom -> crop
         # = rotate + slide -> -pan -> crop (antizoomed canvas frame) -> zoom
         # = rotate + slide -> crop (antizoom canvas frame + pan) -> zoom
-        if self.fixedImagePil == None or not self.fixedImageQueue.empty():
-            (i, p, z) = self.fixedImageQueue.get()
-            while not self.fixedImageQueue.empty():
-                (i, p, z) = self.fixedImageQueue.get()
-            print("read new image", z)
-            self.fixedImagePil = i
-            self.fixedPortionPosition = p
-            self.fixedImageZoom = z
-        # size on screen
+        if self.fixed.pil == None or not self.fixed.queue.empty():
+            (i, p, z) = self.fixed.queue.get()
+            while not self.fixed.queue.empty():
+                (i, p, z) = self.fixed.queue.get()
+            print("read new fixed image", z)
+            self.fixed.pil = i
+            self.fixed.pilPosition = p
+            self.fixed.zoom = z
+        if self.sliding.pil == None or not self.sliding.queue.empty():
+            (i, p, z) = self.sliding.queue.get()
+            while not self.sliding.queue.empty():
+                (i, p, z) = self.sliding.queue.get()
+            print("read new sliding image", z)
+            self.sliding.pil = i
+            self.sliding.pilPosition = p
+            self.sliding.zoom = z
         width = self.canvas.winfo_width()
         height = self.canvas.winfo_height()
-        # position on the fixed background
-        zwidth = (width * 100) / self.zoom
-        zheight = (height * 100) / self.zoom
-        zleft = math.floor(self.panX - zwidth / 2 + 0.5)
-        ztop = math.floor(self.panY - zheight / 2 + 0.5)
-        zwidth = math.floor(zwidth)
-        zheight = math.floor(zheight)
-        # position of cropped image on screen
-        offsetX = 0
-        offsetY = 0
-        # crops in background pixels
-        cropped = False
-        fibb = self.fixedImage.get_mosaic_bounding_box()
-        leftCrop = zleft - self.fixedPortionPosition[0]
-        if leftCrop < 0:
-            offsetX = -leftCrop
-            leftCrop = 0
-            cropped = fibb.x < self.fixedPortionPosition[0]
-        topCrop = ztop - self.fixedPortionPosition[1]
-        if topCrop < 0:
-            offsetY = -topCrop
-            topCrop = 0
-            cropped = cropped or fibb.y < self.fixedPortionPosition[1]
-        rightCrop = zleft + zwidth - self.fixedPortionPosition[0]
-        if self.fixedPortionPosition[2] < rightCrop:
-            rightCrop = self.fixedPortionPosition[2]
-            cropped = cropped or self.fixedPortionPosition[0] + self.fixedPortionPosition[2] < fibb.x + fibb.w
-        bottomCrop = ztop + zheight - self.fixedPortionPosition[1]
-        if self.fixedPortionPosition[3] < bottomCrop:
-            bottomCrop = self.fixedPortionPosition[3]
-            cropped = cropped or self.fixedPortionPosition[1] + self.fixedPortionPosition[3] < fibb.y + fibb.h
-        widthCrop = rightCrop - leftCrop
-        heightCrop = bottomCrop - topCrop
-        # width and height in screen pixels
-        canvasX = math.floor(offsetX * self.zoom / 100)
-        canvasY = math.floor(offsetY * self.zoom / 100)
-        canvasW = math.ceil(widthCrop * self.zoom / 100)
-        canvasH = math.ceil(heightCrop * self.zoom / 100)
-        # crops in extracted image pixels
-        topIm = max(0, math.floor(topCrop * self.fixedImageZoom / 100))
-        bottomIm = min(self.fixedImagePil.height, math.floor(bottomCrop * self.fixedImageZoom / 100))
-        leftIm = max(0, math.floor(leftCrop * self.fixedImageZoom / 100))
-        rightIm = min(self.fixedImagePil.width, math.floor(rightCrop * self.fixedImageZoom / 100))
-        image = self.fixedImagePil.resize(
-            (canvasW, canvasH),
-            resample=Image.NEAREST,
-            box=(leftIm, topIm, rightIm, bottomIm)
+        tim = TransformableImage(self.fixed, self.panX, self.panY, self.zoom)
+        image = tim.get(width, height)
+        sim = TransformableImage(
+            self.sliding,
+            self.panX - self.slideX,
+            self.panY - self.slideY,
+            self.zoom
         )
+        slider = sim.get(width, height)
+        image = Image.blend(image, slider, 0.5)
         self.image = ImageTk.PhotoImage(image)
-        self.canvas.coords(self.canvasImage, canvasX, canvasY)
         self.canvas.itemconfigure(self.canvasImage, image=self.image)
-        magnification = self.zoom / self.fixedImageZoom
-        if (magnification <= 0.5
-            or (2 <= magnification and self.fixedImageZoom < 100)
-            or cropped):
+        if tim.outdated:
             self.readCurrentLocality()
 
 if len(sys.argv) != 3:
